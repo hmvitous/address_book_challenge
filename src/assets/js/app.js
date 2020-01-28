2f9f73df694fef6bb1cce37e68ed8cd443c1dd25
Hunter V <hmvitous@yahoo.com>
2020-01-28 16:59:01 +0100

    Adding function to the add contact button. Creating tests for toggling visibility of new contact form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -28,7 +28,18 @@ const renderContacts = () => {
 document.addEventListener('DOMContentLoaded', () => {
     renderContacts()
     const contactForm = document.getElementById('new-contact-form')
-    contactForm.addEventListener('submit', event => {
+    const toggleFormVisibilityButton = document.getElementById('add-contact')
+    contactForm.style.display = 'none'
+
+    toggleFormVisibilityButton.addEventListener('click', () => {
+        if (contactForm.style.display === '') {
+            contactForm.style.display = 'none'
+        } else {
+            contactForm.style.display = ''
+        }
+    })
+    
+     contactForm.addEventListener('submit', event => {
         event.preventDefault()
 
         //1.Read all of the input fields and get their values
